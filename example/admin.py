c76a1b35f0cce6abee464858c883b24ff4e1790a
gabbork <gabriele@20tab.com>
2017-03-09 09:49:50 +0100

    multi place example

--- example/admin.py
+++ example/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MyLocation
+from .models import MyLocation, MyLocationMultiPlace
 
 
 class MyLocationAdmin(admin.ModelAdmin):
@@ -10,4 +10,10 @@ class MyLocationAdmin(admin.ModelAdmin):
     list_display = ('place', country)
 
 
+class MyLocationMultiPlaceAdmin(admin.ModelAdmin):
+
+    list_display = ('place1', 'place2')
+
+
 admin.site.register(MyLocation, MyLocationAdmin)
+admin.site.register(MyLocationMultiPlace, MyLocationMultiPlaceAdmin)
--- example/models.py
+++ example/models.py
@@ -6,3 +6,14 @@ class MyLocation(models.Model):
     place = PlaceField(
         on_delete=models.SET_NULL,
         null=True, blank=True)
+
+
+class MyLocationMultiPlace(models.Model):
+    place1 = PlaceField(
+        on_delete=models.SET_NULL,
+        related_name="mylocation_places1",
+        null=True, blank=True)
+    place2 = PlaceField(
+        on_delete=models.SET_NULL,
+        related_name="mylocation_places2",
+        null=True, blank=True)
