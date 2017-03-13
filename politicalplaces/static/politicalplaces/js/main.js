d1fb1d506cfd552a08fc3780f99ac6d55a0e8a79
Giacomo Herrero <giacomoherrero@gmail.com>
2017-03-13 18:41:59 +0100

    Cleanup

--- politicalplaces/static/politicalplaces/js/main.js
+++ politicalplaces/static/politicalplaces/js/main.js
@@ -1,10 +1,3 @@
 $(function($) {
-
   politicalplaces.init();
-  $(document).on('formset:added', function(event, $row, formsetName) {
-          
-              console.log(evt, $row, formsetName);
-          
-      });
-
 });
