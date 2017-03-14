b4f656e9de121f089bd29a949196fcbf9058ad16
gabbork <gabriele@20tab.com>
2017-03-14 18:14:59 +0100

    fix tests

--- politicalplaces/tests.py
+++ politicalplaces/tests.py
@@ -20,36 +20,21 @@ class PlaceWidgetTest(TestCase):
         self.assertHTMLEqual(
             str(self.placewidget.media),
             """<link href="/static/politicalplaces/css/politicalplaces.css" type="text/css" media="all" rel="stylesheet" />
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script type="text/javascript" src="/static/politicalplaces/js/politicalplaces.js"></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?language=en&amp;key=AIzaSyCQbVeTVyl1eUDzJbgpoSTaCb_9wHq1kik"></script>"""
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?language=en&amp;key=AIzaSyBUSalEfGXgegMLzcZGSx45YT01okoRfOs"></script>"""
         )
 
     def test_place_widget_render(self):
         self.assertHTMLEqual(
             str(self.placewidget.render(
                 'myfield', 'myvalue', attrs={'id': 'idtest'})),
-            """<input id='idtest' name="myfield" type="text" value="myvalue" /><div class='place-search' id="search_map_idtest">search</div>
-<div class="place-widget" style="margin-top: 4px">
-    <div class='place-panel' id="panel_idtest"></div>
-    <div class='place-map-canvas' id="map_canvas_idtest" style="height: 300px; width: 500px;"></div>
+            """<div class="widget" data-id="idtest" data-name="myfield">
+            <input id='idtest' name="myfield" type="text" value="myvalue" />
+            <a class='widget__search button' id="search_map_idtest">Search</a>
+<div class="widget__place">
+    <div class='widget__place__panel' id="panel_idtest"></div>
+    <div class='widget__place__map' id="map_canvas_idtest"></div>
 </div>
-
-<script>
-jQuery(function($) {
-  function setVariables(field_id, field_name){
-    // not working with multiple PlaceFields.
-    // Probably needing an array of variables or something...
-    panel = $('#panel_'+field_id);
-    map_canvas = document.getElementById('map_canvas_'+field_id);
-    search_map = $('#search_map_'+field_id);
-    address_input = $('#id_'+field_name);
-  }
-
-  setVariables('idtest', 'myfield');
-  initMap();
-})
-</script>
 """
         )
 
